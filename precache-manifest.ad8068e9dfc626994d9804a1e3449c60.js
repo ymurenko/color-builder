0ad6806c2a7d6708b67c790dd6addeb990bdae77
ymurenko <murenko.yuri@gmail.com>
2020-03-15 01:20:11 -0400

    Updates

---
+++ precache-manifest.ad8068e9dfc626994d9804a1e3449c60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03e968d337cf0f804765b368660c9c61",
+    "url": "/color-builder/index.html"
+  },
+  {
+    "revision": "73cc0995283c53b7ca25",
+    "url": "/color-builder/static/css/main.5030d8af.chunk.css"
+  },
+  {
+    "revision": "4d931cde6995b77dd5a8",
+    "url": "/color-builder/static/js/2.7b6b13a5.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/color-builder/static/js/2.7b6b13a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73cc0995283c53b7ca25",
+    "url": "/color-builder/static/js/main.e7dacd74.chunk.js"
+  },
+  {
+    "revision": "bc7ab478b74c30c4d998",
+    "url": "/color-builder/static/js/runtime-main.0bd34440.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/color-builder/static/media/logo.ee7cd8ed.svg"
+  }
+]);
