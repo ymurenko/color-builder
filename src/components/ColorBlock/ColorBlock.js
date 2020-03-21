e4617f19deea2b7bb3c5c11d094eca1ebae6e076
ymurenko <murenko.yuri@gmail.com>
2020-03-21 13:58:44 -0400

    add click to copy color

--- src/components/ColorBlock/ColorBlock.js
+++ src/components/ColorBlock/ColorBlock.js
@@ -3,36 +3,53 @@ import { connect } from "react-redux";
 import copy from "copy-to-clipboard";
 import "./ColorBlock.scss";
 
-const ColorBlock_ = (props) => {
-  const hover = useRef(null)
-  let height = 400 / props.selectorCount
+const ColorBlock_ = props => {
+  const colorBlock = useRef(null);
+  let height = 400 / props.selectorCount;
   let textColor = props.lightness < 50 ? "#bdbdbd" : "#404040";
-  let accentColor = props.lightness < 50 ? "#404040" :  "hsla(0,0%,100%,0.8)";
+
+  const copyColor = event => {
+    let color = props.color;
+    if (!props.hash) {
+      color = color.substr(1);
+    }
+    if (props.quotes) {
+      color = `'${color}'`;
+    }
+    copy(color);
+  };
+
   return (
     <div
+      ref={colorBlock}
       className="color-block"
       style={{
         height: height,
         color: textColor,
         backgroundColor: props.color
-
       }}
+      onClick={event => copyColor(event)}
     >
-      
-      <div className="edit-prompt" ref={hover} style={{ height: height}}></div>
-      <div className="copy-prompt" style={{height: height, paddingTop: `${(height-20)/2}px`}}>{props.color}</div>
+      <div
+        className="copy-prompt"
+        style={{ height: height, paddingTop: `${(height - 20) / 2}px` }}
+      >
+        <p className="color-text hover-cta">{props.color}</p>
+        <p className="copy-text hover-cta">Click to copy</p>
+      </div>
     </div>
   );
 };
 
 function mapStateToProps(state) {
-    return {
-      lightness: state.actionReducer.LIGHTNESS,
-      selectorCount: state.actionReducer.SELECTOR_COUNT
-    };
-  }
-  
-  const ColorBlock = connect(mapStateToProps)(ColorBlock_);
-  
-  export default ColorBlock;
-  
+  return {
+    lightness: state.actionReducer.LIGHTNESS,
+    selectorCount: state.actionReducer.SELECTOR_COUNT,
+    hash: state.actionReducer.HASH,
+    quotes: state.actionReducer.QUOTES
+  };
+}
+
+const ColorBlock = connect(mapStateToProps)(ColorBlock_);
+
+export default ColorBlock;
